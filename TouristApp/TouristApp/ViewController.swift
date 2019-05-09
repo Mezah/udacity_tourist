57fcd1c063e8a5437e306e958ccf5b3a74c0a23f
Hazem <hazem@islam-mohamed-mac.local>
2019-05-09 14:14:27 -0700

    First Commit

---
+++ TouristApp/TouristApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TouristApp
+//
+//  Created by Hazem on 5/9/19.
+//  Copyright © 2019 Hazem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
