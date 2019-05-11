62d5bd4e8955a2e535a65471a0e085b4e0ab58bf
Hazem <hazem@islam-mohamed-mac.local>
2019-05-11 16:08:06 -0700

    Handle user selection from map

--- TouristApp/TouristApp/MapViewController.swift
+++ TouristApp/TouristApp/MapViewController.swift
@@ -9,14 +9,85 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController , MKMapViewDelegate{
 
+    private let dataController = DataController.shared
+    var locationAnnotations = [MKPointAnnotation]()
+    
     @IBOutlet weak var travellerMap: MKMapView!
+    
+    fileprivate func configureMapTouchGesture() {
+        let longPressRecogniser = UILongPressGestureRecognizer(target: self, action: #selector(MapViewController.handleLongPress(_:)))
+        longPressRecogniser.minimumPressDuration = 1.0
+        travellerMap.addGestureRecognizer(longPressRecogniser)
+    }
+    
+    @objc func handleLongPress(_ gestureRecognizer : UIGestureRecognizer){
+        if gestureRecognizer.state != .began { return }
+        
+        let touchPoint = gestureRecognizer.location(in: travellerMap)
+        let touchMapCoordinate = travellerMap?.convert(touchPoint, toCoordinateFrom: travellerMap)
+    
+        if let selectedLocation = touchMapCoordinate {
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = selectedLocation
+           travellerMap.addAnnotation(annotation)
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        travellerMap.delegate = self
+       
+        configureMapTouchGesture()
+        prepareData()
+    }
+    
+    
+    
+    private func prepareData(){
+        // TODO first load data from local storage if any and associate it with the location annotation dictionary
+        
     }
 
 
+    func mapView(_ mapView: MKMapView, didAdd views: [MKAnnotationView]) {
+        //TODO first : Add annotation assocciated to the selected location to the map
+        for view in views {
+            let coordinate = view.annotation?.coordinate
+            let testingMsg = "long = \(coordinate?.longitude), lat = \(coordinate?.latitude)"
+            print(testingMsg)
+        }
+        //TODO Second: Save the selected locations into local storage
+    }
+    // setup annotation view (pins) on map
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinTintColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+
+    // This delegate method is implemented to respond to taps. It should open the photo album related to this pin
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control == view.rightCalloutAccessoryView {
+            if let toOpen = view.annotation {
+                // TODO go to photo album related to this pin
+            }
+        }
+    }
 }
 
---
+++ TouristApp/TouristApp/model/DataController.swift
@@ -0,0 +1,34 @@
+//
+//  DataController.swift
+//  TouristApp
+//
+//  Created by Hazem on 5/11/19.
+//  Copyright © 2019 Hazem. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class DataController {
+    static let shared = DataController(modelName: "TouristAppModel")
+    
+    let persistentContainer:NSPersistentContainer
+    
+    var viewContext:NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    private init(modelName:String) {
+        persistentContainer = NSPersistentContainer(name:modelName)
+    }
+    
+    func load(completion:(()->Void)? = nil ){
+        persistentContainer.loadPersistentStores{ storeDescription,error in
+            
+            guard error == nil else {
+                fatalError(error!.localizedDescription)
+            }
+            completion?	()
+        }
+    }
+}
